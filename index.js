85142e4cd14ecc01aab1e668a9143e97e314cff1
shahinexy <shahinexy@gmail.com>
2024-05-03 23:46:32 +0600

    user post

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // midlewear 
-// app.get(cors())
+app.use(cors())
 
-const corsConfig = {
-    origin: ["http://localhost:5173"],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-};
-app.use(cors(corsConfig));
+// const corsConfig = {
+//   origin: ["http://localhost:5173"],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE']
+// };
+// app.use(cors(corsConfig));
 
-app.get(express.json())
+app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.76h69in.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,10 +34,33 @@ async function run() {
     await client.connect();
 
     const carCollection = client.db("carDoctor").collection('services');
+    const checkOutCollection = client.db("carDoctor").collection('checkOut');
 
-    app.get('/services', async (req, res) =>{
-        const result = await carCollection.find().toArray();
-        res.send(result)
+    app.get('/services', async (req, res) => {
+      const result = await carCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        projection: { title: 1, img: 1, price: 1 }
+      };
+
+      const result = await carCollection.findOne(query, options);
+      res.send(result)
+    })
+
+
+    // check out
+    app.post('/checkOut', async (req, res) => {
+      const checkout = req.body;
+      console.log(checkout);
+      const result = await checkOutCollection.insertOne(checkout);
+      console.log(checkout, result);
+      res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
@@ -51,10 +74,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req ,  res)=>{
-    res.send("Car Doctor server running")
+app.get('/', (req, res) => {
+  res.send("Car Doctor server running")
 })
 
-app.listen(port, ()=>{
-    console.log(`Car Doctor server is running on Port: ${port}`);
+app.listen(port, () => {
+  console.log(`Car Doctor server is running on Port: ${port}`);
 })
