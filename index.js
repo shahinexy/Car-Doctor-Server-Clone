bdcf3cb553e1a76232f1bb2e6346f32676c35f2b
shahinexy <shahinexy@gmail.com>
2024-05-03 22:12:01 +0600

    coreConfig added

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // midlewear 
-app.get(cors())
+// app.get(cors())
+
+const corsConfig = {
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+};
+app.use(cors(corsConfig));
+
 app.get(express.json())
 
 
